9b30ae7827f10a0c0acccd5cd0f5b3af4d523426
raynerjesushuamani-code <raynerjesushuamani@gmail.com>
2026-05-21 10:16:30 -0500

    clases de opraciones con numeros python

---
+++ 03 TIPOS_DATOS/07operaciones.py
@@ -0,0 +1,20 @@
+# suma - operador binario
+## son datos que se pueden utilizar en cualquier parte del software que estes cosmtruyendo
+## variables locales 
+## son datos que solo son accecibles en pequñas porciones de codigo scope
+firts_numb:int|float=20
+second_numb:int|float=5
+# resultado=firts_numb+second
+print(f"la suma de {firts_numb }+{second_numb}={firts_numb+second_numb}")
+#dici
+print(f"la divicion de {firts_numb }/{second_numb}={firts_numb/second_numb}")
+# multiplicacion
+print(f"la multiplicacionj de {firts_numb }*{second_numb}={firts_numb*second_numb}")
+# divicion exacta
+print(f"la divicionexacta de {firts_numb }//{second_numb}={firts_numb//second_numb}")
+# incremento (++,+=)  ojo. esta es un abreviatura de expresion u operacion aritmetica no es un operador de incremnete (numero=numero1(numerp+1) , numero+1=
+print(f"el incremento de {firts_numb} es {++firts_numb}")
+#decremento (--,+=) (numerp-1) , numero-1=
+print(f"el decrememto de {firts_numb} es {--firts_numb}")
+# potenciacion
+print(f"la poten de {firts_numb }**{second_numb}={firts_numb**second_numb}")
